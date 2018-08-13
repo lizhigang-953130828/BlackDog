614f2b5f523aae9b851fa69b130db35387d6aa54
Your Name <you@example.com>
2018-08-13 10:42:45 +0800

    去掉xxx

--- BlackDog/api/src/main/java/com/lizhigang/api/aliyun/SmsApi.java
+++ BlackDog/api/src/main/java/com/lizhigang/api/aliyun/SmsApi.java
@@ -48,7 +48,7 @@ public class SmsApi {
         //必填:待发送手机号
         request.setPhoneNumbers(tel);
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("李志刚");
+        request.setSignName("xxx");
         //必填:短信模板-可在短信控制台中找到
         request.setTemplateCode("SMS_142020221");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
